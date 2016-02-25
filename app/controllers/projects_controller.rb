c32ac36fb2b5ebb6da9a73426e6f9b382ef4c3d2
NgocSon2412 <sonnguyenk57@gmail.com>
2016-02-25 16:02:47 +0700

    delete project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -36,6 +36,12 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def destroy
+    Project.find(params[:id]).destroy
+    flash[:success] = "Project deleted"
+    redirect_to projects_url
+  end
+
   private 
 
   	def project_params
