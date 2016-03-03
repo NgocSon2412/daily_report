44496cc90c6b2e94312c8e391cff50b93fb90d27
HustMaroon <thanhbk6294@gmail.com>
2016-03-03 09:17:15 +0700

    report fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,23 +3,22 @@ class UsersController < ApplicationController
   def new
   end
   def report
-    @token = auth(params[:code]) 
-    user = get_user(@token)
+    @@token = auth(params[:code]) 
+    user = get_user(@@token)
     #add_user(user)
     @user_name = user["user"]
     @project = Project.first
   end
   def report2
-  	@token = auth(params[:code]) 
   	@project = Project.find(params[:project][:id])
   	project_name = @project.project_name
   	text = "[Project_name:]\n#{project_name}\n[Done:]\n#{params[:project][:hours]} hours\n[Problem:]\n#{params[:project][:problem]}\n[Next_plan:]\n#{params[:project][:next_plan]}"
   	@project.real_time += params[:project][:hours].to_i
-    if @project.update_attributes(user_project_params)
-     @res = post_report(text, @token).is_a?(Net::HTTPSuccess)
+  	@res = post_report(text, @@token)
      if @res.is_a?(Net::HTTPSuccess)
-     flash.now[:success] = "Successfully reported!"
+     flash.now[:success] = "#Successfully reported!"
      end
+    if @project.update_attributes(user_project_params)
      render 'thank'
     else
       render 'report'
